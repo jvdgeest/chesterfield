9a47891069f187e6890e7e0d9e8291f05f43efc8
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-04-10 14:02:39 +0200

    Add more CouchClient tests

--- Chesterfield.Tests/TheCouchClientShould.cs
+++ Chesterfield.Tests/TheCouchClientShould.cs
@@ -3,11 +3,11 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MindTouch.Tasking;
 using MindTouch.Dream;
 using Newtonsoft.Json.Linq;
-using System.Configuration;
 using Chesterfield.Interfaces;
 using Chesterfield.Support;
 
@@ -57,6 +57,36 @@ namespace Chesterfield.IntegrationTest
       // Assert
       Assert.IsFalse(client.HasDatabase(baseDatabase));
       Assert.IsFalse(client.HasDatabase(replicateDatabase));
+    }
+
+    [TestMethod]
+    public void ReturnListWithDatabases()
+    {
+      // Arrange
+      IEnumerable<string> databases = client.GetAllDatabases();
+
+      // Assert
+      Assert.IsNotNull(databases);
+      Assert.IsTrue(databases.Contains(baseDatabase));
+      Assert.IsTrue(databases.Contains(replicateDatabase));
+    }
+
+    [TestMethod]
+    public void ReturnDatabaseInformation()
+    {
+      // Arrange
+      CouchDatabase database = client.GetDatabase(baseDatabase);
+      CouchDatabaseInfo couchDatabaseInfo = database.GetInfo();
+
+      // Assert
+      Assert.AreEqual(baseDatabase, couchDatabaseInfo.Name);
+      Assert.AreEqual(false, couchDatabaseInfo.CompactRunning);
+      Assert.AreNotEqual(0, couchDatabaseInfo.DiskFormatVersion);
+      Assert.AreNotEqual(0, couchDatabaseInfo.DiskSize);
+      Assert.AreEqual(0, couchDatabaseInfo.DocCount);
+      Assert.AreEqual(0, couchDatabaseInfo.DocDeletedCount);
+      Assert.AreNotEqual(0, couchDatabaseInfo.InstanceStartTimeMs);
+      Assert.AreNotEqual(DateTime.MinValue, couchDatabaseInfo.InstanceStartTime);
     }
   }
 }
