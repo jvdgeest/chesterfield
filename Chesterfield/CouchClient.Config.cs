5baff17e70c4049a0e0ed2d8f8520f4dc072d03d
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-04-12 19:53:41 +0200

    Add code commenting to CouchClient.Config.cs

--- Chesterfield/CouchClient.Config.cs
+++ Chesterfield/CouchClient.Config.cs
@@ -13,6 +13,12 @@ namespace Chesterfield
      * Asynchronous methods 
      * =======================================================================*/
 
+    /// <summary>
+    /// <para>Retrieves the entire CouchDB server configuration.</para>
+    /// <para>(This method is asynchronous.)</para>
+    /// </summary>
+    /// <param name="result"></param>
+    /// <returns></returns>
     public Result<Dictionary<string, Dictionary<string, string>>> 
       GetConfig(Result<Dictionary<string, Dictionary<string, string>>> result)
     {
@@ -36,6 +42,14 @@ namespace Chesterfield
       return result;
     }
 
+    /// <summary>
+    /// <para>Retrieves a single section from the CouchDB server configuration.
+    /// </para>
+    /// <para>(This method is asynchronous.)</para>
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <param name="result"></param>
+    /// <returns></returns>
     public Result<Dictionary<string, string>> GetConfigSection(
       string section, 
       Result<Dictionary<string, string>> result)
@@ -70,6 +84,15 @@ namespace Chesterfield
       return result;
     }
 
+    /// <summary>
+    /// <para>Retrieves a single configuration value from a given section in the
+    /// CouchDB server configuration.</para>
+    /// <para>(This method is asynchronous.)</para>
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <param name="keyName">The name of the configuration key.</param>
+    /// <param name="result"></param>
+    /// <returns></returns>
     public Result<string> GetConfigValue(
       string section, 
       string keyName,
@@ -109,6 +132,16 @@ namespace Chesterfield
       return result;
     }
 
+    /// <summary>
+    /// <para>Sets a single configuration value for a given section in the
+    /// CouchDB server configuration.</para>
+    /// <para>(This method is asynchronous.)</para>
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <param name="keyName">The name of the configuration key.</param>
+    /// <param name="value">The value to set.</param>
+    /// <param name="result"></param>
+    /// <returns></returns>
     public Result SetConfigValue(
       string section, 
       string keyName, 
@@ -143,6 +176,15 @@ namespace Chesterfield
       return result;
     }
 
+    /// <summary>
+    /// <para>Deletes a single configuration value from a given section in the
+    /// CouchDB server configuration.</para>
+    /// <para>(This method is asynchronous.)</para>
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <param name="keyName">The name of the configuration key.</param>
+    /// <param name="result"></param>
+    /// <returns></returns>
     public Result DeleteConfigValue(
       string section, 
       string keyName, 
@@ -176,28 +218,59 @@ namespace Chesterfield
      * Synchronous methods 
      * =======================================================================*/
 
+    /// <summary>
+    /// Retrieves the entire CouchDB server configuration.
+    /// </summary>
+    /// <returns>A dictionary containing the configuration sections. Each 
+    /// section has its keys and values stored in a dictionary.</key>
+    /// </returns>
     public Dictionary<string, Dictionary<string, string>> GetConfig()
     {
       return GetConfig(new Result<Dictionary<string, 
         Dictionary<string, string>>>()).Wait();
     }
 
+    /// <summary>
+    /// Retrieves a single section from the CouchDB server configuration.
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <returns>A dictionary containing the keys and values.</returns>
     public Dictionary<string, string> GetConfig(string section)
     {
       return GetConfigSection(section, 
         new Result<Dictionary<string, string>>()).Wait();
     }
 
+    /// <summary>
+    /// Retrieves a single configuration value from a given section in the
+    /// CouchDB server configuration.
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <param name="keyName">The name of the configuration key.</param>
+    /// <returns></returns>
     public string GetConfigValue(string section, string keyName)
     {
       return GetConfigValue(section, keyName, new Result<string>()).Wait();
     }
 
+    /// <summary>
+    /// Sets a single configuration value for a given section in the CouchDB 
+    /// server configuration.
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <param name="keyName">The name of the configuration key.</param>
+    /// <param name="value">The value to set.</param>
     public void SetConfigValue(string section, string keyName, string value)
     {
       SetConfigValue(section, keyName, value, new Result()).Wait();
     }
 
+    /// <summary>
+    /// Deletes a single configuration value from a given section in the CouchDB 
+    /// server configuration.
+    /// </summary>
+    /// <param name="section">The configuration section.</param>
+    /// <param name="keyName">The name of the configuration key.</param>
     public void DeleteConfigValue(string section, string keyName)
     {
       DeleteConfigValue(section, keyName, new Result()).Wait();
