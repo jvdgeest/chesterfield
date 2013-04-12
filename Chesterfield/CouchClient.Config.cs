f8fb9fbf9c2abef9c7753620f3c72be1e81086db
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-04-12 19:08:47 +0200

    Refactor CouchClient config methods into own partial class

---
+++ Chesterfield/CouchClient.Config.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using MindTouch.Dream;
+using MindTouch.Tasking;
+using Chesterfield.Support;
+
+namespace Chesterfield
+{
+  public partial class CouchClient : CouchBase
+  {
+    /* =========================================================================
+     * Asynchronous methods 
+     * =======================================================================*/
+
+    public Result<Dictionary<string, Dictionary<string, string>>> 
+      GetConfig(Result<Dictionary<string, Dictionary<string, string>>> result)
+    {
+      if (result == null)
+        throw new ArgumentNullException("result");
+
+      BasePlug
+        .At(Constants._CONFIG)
+        .Get(DreamMessage.Ok(), new Result<DreamMessage>())
+        .WhenDone(
+          a =>
+          {
+            if (a.Status == DreamStatus.Ok)
+              result.Return(JsonConvert.DeserializeObject<Dictionary<
+                string, Dictionary<string, string>>>(a.ToText()));
+            else
+              result.Throw(new CouchException(a));
+          },
+          result.Throw
+        );
+      return result;
+    }
+
+    public Result<Dictionary<string, string>> GetConfigSection(
+      string section, 
+      Result<Dictionary<string, string>> result)
+    {
+      if (String.IsNullOrEmpty(section))
+        throw new ArgumentException("section cannot be null nor empty");
+      if (result == null)
+        throw new ArgumentNullException("result");
+
+      BasePlug
+        .At(Constants._CONFIG, XUri.EncodeFragment(section))
+        .Get(DreamMessage.Ok(), new Result<DreamMessage>())
+        .WhenDone(
+          a =>
+          {
+            switch (a.Status)
+            {
+              case DreamStatus.Ok:
+                result.Return(JsonConvert.DeserializeObject<
+                  Dictionary<string, string>>(a.ToText()));
+                break;
+              case DreamStatus.NotFound:
+                result.Return(new Dictionary<string, string>());
+                break;
+              default:
+                result.Throw(new CouchException(a));
+                break;
+            }
+          },
+          result.Throw
+        );
+      return result;
+    }
+
+    public Result<string> GetConfigValue(
+      string section, 
+      string keyName,
+      Result<string> result)
+    {
+      if (String.IsNullOrEmpty(section))
+        throw new ArgumentException("section cannot be null nor empty");
+      if (String.IsNullOrEmpty(keyName))
+        throw new ArgumentException("keyName cannot be null nor empty");
+      if (result == null)
+        throw new ArgumentNullException("result");
+
+      BasePlug
+        .At(Constants._CONFIG, XUri.EncodeFragment(section), 
+            XUri.EncodeFragment(keyName))
+        .Get(DreamMessage.Ok(), new Result<DreamMessage>())
+        .WhenDone(
+          a =>
+          {
+            string value = a.ToText();
+            switch (a.Status)
+            {
+              case DreamStatus.Ok:
+                // remove " and "\n
+                result.Return(value.Substring(1, value.Length - 3));
+                break;
+              case DreamStatus.NotFound:
+                result.Return((string)null);
+                break;
+              default:
+                result.Throw(new CouchException(a));
+                break;
+            }
+          },
+          result.Throw
+        );
+      return result;
+    }
+
+    public Result SetConfigValue(
+      string section, 
+      string keyName, 
+      string value, 
+      Result result)
+    {
+      if (String.IsNullOrEmpty(section))
+        throw new ArgumentException("section cannot be null nor empty");
+      if (String.IsNullOrEmpty(keyName))
+        throw new ArgumentException("keyName cannot be null nor empty");
+      if (result == null)
+        throw new ArgumentNullException("result");
+
+      if (value == null)
+        return DeleteConfigValue(section, keyName, result);
+
+      BasePlug
+        .At(Constants._CONFIG, XUri.EncodeFragment(section), 
+            XUri.EncodeFragment(keyName))
+        .Put(DreamMessage.Ok(MimeType.TEXT, "\"" + value + "\""), 
+             new Result<DreamMessage>())
+        .WhenDone(
+          a =>
+          {
+            if (a.Status == DreamStatus.Ok)
+              result.Return();
+            else
+              result.Throw(new CouchException(a));
+          },
+          result.Throw
+        );
+      return result;
+    }
+
+    public Result DeleteConfigValue(
+      string section, 
+      string keyName, 
+      Result result)
+    {
+      if (String.IsNullOrEmpty(section))
+        throw new ArgumentException("section cannot be null nor empty");
+      if (String.IsNullOrEmpty(keyName))
+        throw new ArgumentException("keyName cannot be null nor empty");
+      if (result == null)
+        throw new ArgumentNullException("result");
+
+      BasePlug
+        .At(Constants._CONFIG, XUri.EncodeFragment(section), 
+            XUri.EncodeFragment(keyName))
+        .Delete(DreamMessage.Ok(), new Result<DreamMessage>())
+        .WhenDone(
+          a =>
+          {
+            if (a.Status == DreamStatus.Ok)
+              result.Return(); // remove " and "\n
+            else
+              result.Throw(new CouchException(a));
+          },
+          result.Throw
+        );
+      return result;
+    }
+
+    /* =========================================================================
+     * Synchronous methods 
+     * =======================================================================*/
+
+    public Dictionary<string, Dictionary<string, string>> GetConfig()
+    {
+      return GetConfig(new Result<Dictionary<string, 
+        Dictionary<string, string>>>()).Wait();
+    }
+
+    public Dictionary<string, string> GetConfig(string section)
+    {
+      return GetConfigSection(section, 
+        new Result<Dictionary<string, string>>()).Wait();
+    }
+
+    public string GetConfigValue(string section, string keyName)
+    {
+      return GetConfigValue(section, keyName, new Result<string>()).Wait();
+    }
+
+    public void SetConfigValue(string section, string keyName, string value)
+    {
+      SetConfigValue(section, keyName, value, new Result()).Wait();
+    }
+
+    public void DeleteConfigValue(string section, string keyName)
+    {
+      DeleteConfigValue(section, keyName, new Result()).Wait();
+    }
+  }
+}
--- Chesterfield/CouchClient.cs
+++ Chesterfield/CouchClient.cs
@@ -11,6 +11,8 @@ namespace Chesterfield
 {
   /* This class has the following partial classes:
    * > CouchClient.cs
+   * > CouchClient.Config.cs
+   * > CouchClient.Databases.cs
    * > CouchClient.Users.cs
    */
   public partial class CouchClient : CouchBase
@@ -46,230 +48,50 @@ namespace Chesterfield
     {
     }
 
+    /* =========================================================================
+     * Asynchronous methods 
+     * =======================================================================*/
+
     /// <summary>
-    /// Triggers one way replication from the source to target.  If bidirection is needed call this method twice with the source and target args reversed.
+    /// <para>Restarts the CouchDB instance. You must be authenticated as a user
+    /// with administration privileges for this to work.</para>
+    /// <para>(This method is asynchronous.)</para>
     /// </summary>
-    /// <param name="aReplicationOptions">Replication Options</param>
-    /// <param name="aResult"></param>
+    /// <param name="result"></param>
     /// <returns></returns>
-    [Obsolete("If using CouchDB >= 1.1 use CouchReplicationDocument")]
-    public Result<JObject> TriggerReplication(ReplicationOptions aReplicationOptions, Result<JObject> aResult)
+    public Result RestartServer(Result result)
     {
-      if (aReplicationOptions == null)
-        throw new ArgumentNullException("aReplicationOptions");
-      if (aResult == null)
-        throw new ArgumentNullException("aResult");
+      if (result == null)
+        throw new ArgumentNullException("result");
 
-      Plug p = BasePlug.At(Constants._REPLICATE);
-
-      string json = aReplicationOptions.ToString();
-      p.Post(DreamMessage.Ok(MimeType.JSON, json), new Result<DreamMessage>()).WhenDone(
-        a =>
-        {
-          if ((a.Status == DreamStatus.Accepted) ||
-             (a.Status == DreamStatus.Ok))
+      BasePlug
+        .At(Constants._RESTART)
+        .Post(DreamMessage.Ok(MimeType.JSON, String.Empty), 
+              new Result<DreamMessage>())
+        .WhenDone(
+          a =>
           {
-            aResult.Return(JObject.Parse(a.ToText()));
-          }
-          else
-          {
-            aResult.Throw(new CouchException(a));
-          }
-        },
-        aResult.Throw
-      );
-
-      return aResult;
-    }
-    /// <summary>
-    /// Restarts the CouchDB instance. You must be authenticated as a user with administration privileges for this to work.
-    /// </summary>
-    /// <param name="aResult"></param>
-    /// <returns></returns>
-    public Result RestartServer(Result aResult)
-    {
-      if (aResult == null)
-        throw new ArgumentNullException("aResult");
-
-      BasePlug.At(Constants._RESTART).Post(DreamMessage.Ok(MimeType.JSON, String.Empty), new Result<DreamMessage>()).WhenDone(
-        a =>
-        {
-          if (a.Status == DreamStatus.Ok)
-            aResult.Return();
-          else
-            aResult.Throw(new CouchException(a));
-        },
-        aResult.Throw
-      );
-      return aResult;
+            if (a.Status == DreamStatus.Ok)
+              result.Return();
+            else
+              result.Throw(new CouchException(a));
+          },
+          result.Throw
+        );
+      return result;
     }
 
+    /* =========================================================================
+     * Synchronous methods 
+     * =======================================================================*/
+
     /// <summary>
-    /// Triggers one way replication from the source to target.  If bidirection is needed call this method twice with the source and target args reversed.
-    /// </summary>
-    /// <param name="aReplicationOptions">Replication options</param>
-    /// <returns></returns>
-    public JObject TriggerReplication(ReplicationOptions aReplicationOptions)
-    {
-      return TriggerReplication(aReplicationOptions, new Result<JObject>()).Wait();
-    }
-    /// <summary>
-    /// Restarts the CouchDB instance. You must be authenticated as a user with administration privileges for this to work.
+    /// Restarts the CouchDB instance. You must be authenticated as a user with 
+    /// administration privileges for this to work.
     /// </summary>
     public void RestartServer()
     {
       RestartServer(new Result()).Wait();
     }
-
-
-    #region Configuration Management
-    #region Asynchronous Methods
-    public Result<Dictionary<string, Dictionary<string, string>>> GetConfig(Result<Dictionary<string, Dictionary<string, string>>> aResult)
-    {
-      if (aResult == null)
-        throw new ArgumentNullException("aResult");
-
-      BasePlug.At(Constants._CONFIG).Get(DreamMessage.Ok(), new Result<DreamMessage>()).WhenDone(
-        a =>
-        {
-          if (a.Status == DreamStatus.Ok)
-            aResult.Return(JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(a.ToText()));
-          else
-            aResult.Throw(new CouchException(a));
-        },
-        aResult.Throw
-      );
-      return aResult;
-    }
-    public Result<Dictionary<string, string>> GetConfigSection(string aSection, Result<Dictionary<string, string>> aResult)
-    {
-      if (aSection == null)
-        throw new ArgumentNullException("aSection");
-      if (aResult == null)
-        throw new ArgumentNullException("aResult");
-      if (String.IsNullOrEmpty(aSection))
-        throw new ArgumentException("Section cannot be empty");
-
-      BasePlug.At(Constants._CONFIG, XUri.EncodeFragment(aSection)).Get(DreamMessage.Ok(), new Result<DreamMessage>()).WhenDone(
-        a =>
-        {
-          switch (a.Status)
-          {
-            case DreamStatus.Ok:
-              aResult.Return(JsonConvert.DeserializeObject<Dictionary<string, string>>(a.ToText()));
-              break;
-            case DreamStatus.NotFound:
-              aResult.Return(new Dictionary<string, string>());
-              break;
-            default:
-              aResult.Throw(new CouchException(a));
-              break;
-          }
-        },
-        aResult.Throw
-      );
-      return aResult;
-    }
-    public Result<string> GetConfigValue(string aSection, string aKeyName, Result<string> aResult)
-    {
-      if (String.IsNullOrEmpty(aSection))
-        throw new ArgumentException("aSection cannot be null nor empty");
-      if (String.IsNullOrEmpty(aKeyName))
-        throw new ArgumentException("aKeyName cannot be null nor empty");
-      if (aResult == null)
-        throw new ArgumentNullException("aResult");
-
-
-      BasePlug.At(Constants._CONFIG, XUri.EncodeFragment(aSection), XUri.EncodeFragment(aKeyName)).Get(DreamMessage.Ok(), new Result<DreamMessage>()).WhenDone(
-        a =>
-        {
-          string value = a.ToText();
-          switch (a.Status)
-          {
-            case DreamStatus.Ok:
-              aResult.Return(value.Substring(1, value.Length - 3));// remove " and "\n
-              break;
-            case DreamStatus.NotFound:
-              aResult.Return((string)null);
-              break;
-            default:
-              aResult.Throw(new CouchException(a));
-              break;
-          }
-        },
-        aResult.Throw
-      );
-      return aResult;
-    }
-    public Result SetConfigValue(string aSection, string aKeyName, string aValue, Result aResult)
-    {
-      if (String.IsNullOrEmpty(aSection))
-        throw new ArgumentException("aSection cannot be null nor empty");
-      if (String.IsNullOrEmpty(aKeyName))
-        throw new ArgumentException("aKeyName cannot be null nor empty");
-      if (aResult == null)
-        throw new ArgumentNullException("aResult");
-
-      if (aValue == null)
-        return DeleteConfigValue(aSection, aKeyName, aResult);
-
-      BasePlug.At(Constants._CONFIG, XUri.EncodeFragment(aSection), XUri.EncodeFragment(aKeyName)).Put(DreamMessage.Ok(MimeType.TEXT, "\"" + aValue + "\""), new Result<DreamMessage>()).WhenDone(
-        a =>
-        {
-          if (a.Status == DreamStatus.Ok)
-            aResult.Return();
-          else
-            aResult.Throw(new CouchException(a));
-        },
-        aResult.Throw
-      );
-      return aResult;
-    }
-    public Result DeleteConfigValue(string aSection, string aKeyName, Result aResult)
-    {
-      if (String.IsNullOrEmpty(aSection))
-        throw new ArgumentException("aSection cannot be null nor empty");
-      if (String.IsNullOrEmpty(aKeyName))
-        throw new ArgumentException("aKeyName cannot be null nor empty");
-      if (aResult == null)
-        throw new ArgumentNullException("aResult");
-
-      BasePlug.At(Constants._CONFIG, XUri.EncodeFragment(aSection), XUri.EncodeFragment(aKeyName)).Delete(DreamMessage.Ok(), new Result<DreamMessage>()).WhenDone(
-        a =>
-        {
-          if (a.Status == DreamStatus.Ok)
-            aResult.Return();// remove " and "\n
-          else
-            aResult.Throw(new CouchException(a));
-        },
-        aResult.Throw
-      );
-      return aResult;
-    }
-    #endregion
-
-    #region Synchronous Methods
-    public Dictionary<string, Dictionary<string, string>> GetConfig()
-    {
-      return GetConfig(new Result<Dictionary<string, Dictionary<string, string>>>()).Wait();
-    }
-    public Dictionary<string, string> GetConfig(string aSection)
-    {
-      return GetConfigSection(aSection, new Result<Dictionary<string, string>>()).Wait();
-    }
-    public string GetConfigValue(string aSection, string aKeyName)
-    {
-      return GetConfigValue(aSection, aKeyName, new Result<string>()).Wait();
-    }
-    public void SetConfigValue(string aSection, string aKeyName, string aValue)
-    {
-      SetConfigValue(aSection, aKeyName, aValue, new Result()).Wait();
-    }
-    public void DeleteConfigValue(string aSection, string aKeyName)
-    {
-      DeleteConfigValue(aSection, aKeyName, new Result()).Wait();
-    }
-    #endregion
-    #endregion
   }
 }
