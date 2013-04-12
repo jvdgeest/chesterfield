e2a9af5d9d26f56b0f5159eaedaa4b80c95be6fe
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-04-12 20:42:26 +0200

    Add source code comments to CouchAttachment.cs

--- Chesterfield/CouchAttachment.cs
+++ Chesterfield/CouchAttachment.cs
@@ -2,23 +2,40 @@
 
 namespace Chesterfield
 {
-  /// <summary>
-  /// Document Attachement
-  /// </summary>
   public class CouchAttachment
   {
+    /// <summary>
+    /// When set to true, this entry will only contain the metadata.
+    /// </summary>
     [JsonProperty(Constants.STUB)]
     public bool Stub { get; internal set; }
 
+    /// <summary>
+    /// Content type of this attachment (for instance "text/plain").
+    /// </summary>
     [JsonProperty(Constants.CONTENT_TYPE)]
     public string ContentType { get; internal set; }
 
+    /// <summary>
+    /// Length of the attachment data in bytes.
+    /// </summary>
     [JsonProperty(Constants.LENGTH)]
     public long Length { get; internal set; }
 
+    /// <summary>
+    /// Base64 data string that must be on a single line of characters.
+    /// </summary>
     [JsonProperty(Constants.DATA)]
     public string Data { get; internal set; }
 
+    /// <summary>
+    /// MD5 hash of the attachment data. Please note that CouchDB compresses 
+    /// some attachments (determined by the content type). "text/plain" is one  
+    /// of them (the full list is in your server's default.ini file), and so the 
+    /// MD5 returned is that of the compressed form. If you supply a digest when
+    /// uploading a standalone attachment, CouchDB will verify it matches and 
+    /// return an error if it doesn't.
+    /// </summary>
     [JsonProperty(Constants.DIGEST)]
     public string Digest { get; internal set; }
   }
