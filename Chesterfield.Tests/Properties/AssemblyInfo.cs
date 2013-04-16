a7065ff909572af7c3f92ff0ed0be47f96cf1bb3
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-04-16 08:29:40 +0200

    Bump up version number to 1.2.1

--- Chesterfield.Tests/Properties/AssemblyInfo.cs
+++ Chesterfield.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DreamSeat.IntegrationTest")]
+[assembly: AssemblyTitle("Chesterfield.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DreamSeat.IntegrationTest")]
+[assembly: AssemblyProduct("Chesterfield.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
