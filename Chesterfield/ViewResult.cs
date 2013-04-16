246c8a1efd3712f2f6fc0b1e0b29b080f90d3986
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-04-16 10:29:39 +0200

    Improve documentation of the ViewResult and ViewResultRow classes

--- Chesterfield/ViewResult.cs
+++ Chesterfield/ViewResult.cs
@@ -5,8 +5,15 @@ using Chesterfield.Interfaces;
 
 namespace Chesterfield
 {
+  /// <summary>
+  /// View result that only contains the total number of rows, offset, ETag and
+  /// HTTP status code. The rows are not included.
+  /// </summary>
   public abstract class BaseViewResult : IBaseViewResult
   {
+    /// <summary>
+    /// Total number of rows that are returned by the view.
+    /// </summary>
     [JsonProperty(Constants.TOTAL_ROWS)]
     public int TotalRows
     {
@@ -14,6 +21,12 @@ namespace Chesterfield
       internal set;
     }
 
+    /// <summary>
+    /// The offset of the first row returned as it exists in the entire view.
+    /// For instance, if you have three documents in your view called a, b and
+    /// c, and query with no startkey then the offset will be 0. If you query
+    /// with startkey="b", then the offset will be 1.
+    /// </summary>
     [JsonProperty(Constants.OFFSET)]
     public int OffSet
     {
@@ -21,16 +34,32 @@ namespace Chesterfield
       internal set;
     }
 
+    /// <summary>
+    /// Entity tag that can be used for web cache validation.
+    /// </summary>
     [JsonIgnore]
     public string ETag { get; internal set; }
 
+    /// <summary>
+    /// The HTTP status code returned by CouchDB (such as 200 OK).
+    /// </summary>
     [JsonIgnore]
     public DreamStatus Status { get; internal set; }
   }
 
+  /// <summary>
+  /// View result that contains the metadata (such as total number of rows and
+  /// offset) and the row data. Each row contains the CouchDB document ID, a key
+  /// (can be null) and a value (can be null).
+  /// </summary>
+  /// <typeparam name="TKey">Object type for the key.</typeparam>
+  /// <typeparam name="TValue">Object type for the value.</typeparam>
   public class ViewResult<TKey, TValue> : BaseViewResult, 
     IViewResult<TKey, TValue>
   {
+    /// <summary>
+    /// List of ViewResultRow objects containing the keys and values.
+    /// </summary>
     [JsonProperty(Constants.ROWS)]
     public IEnumerable<ViewResultRow<TKey, TValue>> Rows
     {
@@ -39,9 +68,23 @@ namespace Chesterfield
     }
   }
 
+  /// <summary>
+  /// View result that contains the metadata (such as total number of rows and
+  /// offset) and the row data. Each row contains the CouchDB document ID, a key
+  /// (can be null), a value (can be null) and the corresponding CouchDB 
+  /// document. You must set the "include_docs" query option to true to make
+  /// this work.
+  /// </summary>
+  /// <typeparam name="TKey">Object type for the key.</typeparam>
+  /// <typeparam name="TValue">Object type for the value.</typeparam>
+  /// <typeparam name="TDocument">Object type for the document (must inherit 
+  /// from ICouchDocument).</typeparam>
   public class ViewResult<TKey, TValue, TDocument> : BaseViewResult, 
     IViewResult<TKey, TValue, TDocument> where TDocument : ICouchDocument
   {
+    /// <summary>
+    /// List of ViewResultRow objects containing the keys, values and documents.
+    /// </summary>
     [JsonProperty(Constants.ROWS)]
     public IEnumerable<ViewResultRow<TKey, TValue, TDocument>> Rows
     {
--- Chesterfield/ViewResultRow.cs
+++ Chesterfield/ViewResultRow.cs
@@ -4,14 +4,26 @@ using Chesterfield.Interfaces;
 
 namespace Chesterfield
 {
+  /// <summary>
+  /// A single view row containg the CouchDB document ID and a key (can be 
+  /// null).
+  /// </summary>
+  /// <typeparam name="TKey">Object type for the key.</typeparam>
   public class ViewResultRow<TKey> : IViewResultRow<TKey>
   {
+    /// <summary>
+    /// ID of the CouchDB document.
+    /// </summary>
     [JsonProperty(Constants.ID)]
     public string Id
     {
       get;
       internal set;
     }
+
+    /// <summary>
+    /// The key that is assigned by the view's map function (can be null).
+    /// </summary>
     [JsonProperty(Constants.KEY)]
     public TKey Key
     {
@@ -19,6 +31,13 @@ namespace Chesterfield
       internal set;
     }
   }
+
+  /// <summary>
+  /// A single view row containing the CouchDB document ID, a key (can be null)
+  /// and a value (can be null).
+  /// </summary>
+  /// <typeparam name="TKey">Object type for the key.</typeparam>
+  /// <typeparam name="TValue">Object type for the value.</typeparam>
   public class ViewResultRow<TKey, TValue> :
     ViewResultRow<TKey>,
     IViewResultRow<TKey, TValue>
@@ -30,6 +49,16 @@ namespace Chesterfield
       internal set;
     }
   }
+
+  /// <summary>
+  /// A single view row containing the CouchDB document ID, a key (can be null),
+  /// a value (can be null) and the corresponding CouchDB document. You must set
+  /// the "include_docs" query option to true to make this work.
+  /// </summary>
+  /// <typeparam name="TKey">Object type for the key.</typeparam>
+  /// <typeparam name="TValue">Object type for the value.</typeparam>
+  /// <typeparam name="TDocument">Object type for the document (must inherit 
+  /// from ICouchDocument).</typeparam>
   public class ViewResultRow<TKey, TValue, TDocument> :
     ViewResultRow<TKey, TValue>,
     IViewResultRow<TKey, TValue, TDocument>
