e18a799b8977fb567a113fe5a17344ec49f7137e
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-04-25 12:36:36 +0200

    Add documentation to IAuditableDocument.cs

--- Chesterfield/Interfaces/IAuditableDocument.cs
+++ Chesterfield/Interfaces/IAuditableDocument.cs
@@ -2,11 +2,34 @@
 {
   public interface IAuditableDocument
   {
+    /// <summary>
+    /// Gets invoked when a document is being created.
+    /// </summary>
     void Creating();
+
+    /// <summary>
+    /// Gets invoked after a document is created.
+    /// </summary>
     void Created();
+
+    /// <summary>
+    /// Gets invoked when a document is being updated.
+    /// </summary>
     void Updating();
+
+    /// <summary>
+    /// Gets invoked when a document is updated.
+    /// </summary>
     void Updated();
+
+    /// <summary>
+    /// Gets invoked when a document is being deleted.
+    /// </summary>
     void Deleting();
+
+    /// <summary>
+    /// Gets invoked when a document is deleted.
+    /// </summary>
     void Deleted();
   }
 }
